15c69502971a52eb9c5341c2463e9ecb4b6401de
Maxwell <Maxwell@aTom-PC>
2016-05-22 17:40:41 +0400

    PasswordManager

---
+++ 1.0.PasswordManager/src/rus/zti/passwordmanager/Print.java
@@ -0,0 +1,10 @@
+package rus.zti.passwordmanager;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println(args[0]);      //запускам cmd далее пишем cd C:/2 далее: C:\2>dir далее C:\2>java -jar a1.jar "» то что хочешь"
+	}
+
+}
