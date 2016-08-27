64495f2c5efe087cde7c23e330e458ce584cc0ca
Maxwell <Maxwell@aTom-PC>
2016-08-27 09:35:57 +0300

    Fxxxx

--- 2.2.JavaFxBuild/src/application/Main.java
+++ 2.2.JavaFxBuild/src/application/Main.java
@@ -1,27 +1,39 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	private Stage stage;
 	private Scene scene;
-	private FlowPane pane;
+	private AnchorPane pane;
 	private Label label;
 	
 	@Override
 	public void start(Stage stage) { //Этот метод из класса апликэйшен запускается при методе Launch(args)
 		
 		this.stage = stage;
-		pane= new FlowPane();
-		label = new Label("Hello epta");
-		pane.getChildren().add(label);  //метод getChildren, как вариант помещения на панель коммпонентов.
-		scene = new Scene(pane, 300, 400);
+		try {
+			
+			//(Анкорпейн) костование так как пейн в данном случае анкорпейн (полиморфизм), а все остальное это чтоб загрузить файл Helloworld.
+			
+			pane= (AnchorPane) FXMLLoader.load(Main.class.getResource("HelloWorld1.fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		
+		scene = new Scene(pane, 700, 300);
 		stage.setScene(scene);
 		stage.show();
 		
---
+++ 2.2.JavaFxBuild/src/application/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Maxwell
+ *
+ */
+package application;
